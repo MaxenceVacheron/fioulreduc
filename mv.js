63e9aa9e6af113838b932dd6e1f164c25f75b039
MaxenceVacheron <vacheronmaxence@gmail.com>
2021-11-13 01:05:35 +0100

    start

---
+++ mv.js
@@ -0,0 +1,8 @@
+function popUpContactInfo() {
+	document.getElementById('infomodal').style.display = 'block';
+}
+
+function closePopUp() {
+	document.getElementById('infomodal').style.display = 'none';
+
+}
